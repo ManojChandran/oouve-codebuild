1812d600dd28a9b29195db1adafb5d5d27ed5e26
ManojChandran <manoj.meparmbu@gmail.com>
2019-12-05 05:22:17 -0500

    added code build

--- main.tf
+++ main.tf
@@ -15,3 +15,139 @@
 provider "aws" {
   region = "${var.aws-region}"
 }
+
+resource "aws_s3_bucket" "oouve-cache" {
+  bucket = "oouve-cache"
+  acl    = "private"
+}
+
+resource "aws_iam_role" "oouve-codebuild-role" {
+  name = "oouve-codebuild-role"
+  tags = {
+    Name = "oouve-codebuild-role"
+  }
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codebuild.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "oouve-codebuild-policy" {
+  role = "${aws_iam_role.oouve-codebuild-role.name}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Resource": [
+        "*"
+      ],
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:CreateNetworkInterface",
+        "ec2:DescribeDhcpOptions",
+        "ec2:DescribeNetworkInterfaces",
+        "ec2:DeleteNetworkInterface",
+        "ec2:DescribeSubnets",
+        "ec2:DescribeSecurityGroups",
+        "ec2:DescribeVpcs"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:CreateNetworkInterfacePermission"
+      ],
+      "Resource": [
+        "arn:aws:ec2:us-east-1:123456789012:network-interface/*"
+      ],
+      "Condition": {
+        "StringEquals": {
+          "ec2:AuthorizedService": "codebuild.amazonaws.com"
+        }
+      }
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:*"
+      ],
+      "Resource": [
+        "${aws_s3_bucket.oouve-cache.arn}",
+        "${aws_s3_bucket.oouve-cache.arn}/*"
+      ]
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_codebuild_project" "oouve-code-build" {
+  name          = "${var.projectname}"
+  build_timeout = "5"
+  service_role  = "${aws_iam_role.oouve-codebuild-role.arn}"
+
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+
+  cache {
+    type     = "S3"
+    location = "${aws_s3_bucket.oouve-cache.bucket}"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:1.0"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+
+    environment_variable {
+      name  = "SOME_KEY1"
+      value = "SOME_VALUE1"
+    }
+  }
+
+  source {
+    type            = "GITHUB"
+    location        = "https://github.com/mitchellh/packer.git"
+    git_clone_depth = 1
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      group_name  = "log-group"
+      stream_name = "log-stream"
+    }
+
+    s3_logs {
+      status   = "ENABLED"
+      location = "${aws_s3_bucket.oouve-cache.id}/build-log"
+    }
+  }
+
+  tags = {
+    Name = "oouve-code-build"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -3,3 +3,6 @@
 variable "aws-region" {
   description = "stored aws region data"
 }
+variable "projectname" {
+  description = "stores project name"
+}
